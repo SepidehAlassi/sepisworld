c63c408a9c84bd822744341bacf8e4db82e9e8a1
Sepideh Alassi <>
2025-02-17 17:24:00 +0100

    init private website

---
+++ pelicanconf.py
@@ -0,0 +1,35 @@
+AUTHOR = 'Sepideh Alassi'
+SITENAME = "Sepi's World"
+SITEURL = ''
+
+PATH = 'content'
+
+TIMEZONE = 'Europe/Rome'
+
+DEFAULT_LANG = 'en'
+
+# Feed generation is usually not desired when developing
+FEED_ALL_ATOM = None
+CATEGORY_FEED_ATOM = None
+TRANSLATION_FEED_ATOM = None
+AUTHOR_FEED_ATOM = None
+AUTHOR_FEED_RSS = None
+
+# Blogroll
+# LINKS = (('Pelican', 'https://getpelican.com/'),
+#          ('Python.org', 'https://www.python.org/'),
+#          ('Jinja2', 'https://palletsprojects.com/p/jinja/'),
+#          ('You can modify those links in your config file', '#'),)
+
+# Contact
+SOCIAL = (('Professional', 'https://dhlab.philhist.unibas.ch/en/persons/sepideh-alassi/'),
+          ('Instagram', 'https://www.instagram.com/sepisworld/'),
+          ('X', 'https://x.com/se_alassi'),
+          ('LinkedIn', 'https://www.linkedin.com/in/sepideh-alassi-phd-04724362/'),)
+
+DEFAULT_PAGINATION = 5
+
+# Uncomment following line if you want document-relative URLs when developing
+RELATIVE_URLS = True
+# SITELOGO = ''
+THEME = "pelican-twitchy"
